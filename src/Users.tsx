7c3caafa779cd89475d2ae9a892495c83d8d85d0
Vijay Pemmaraju <vijay@grooveapp.com>
2022-06-27 10:22:45 -0700

    Always show winner on top

--- src/Users.tsx
+++ src/Users.tsx
@@ -2,12 +2,64 @@ import React, { FC } from 'react';
 import { AnimatePresence, motion, Reorder } from 'framer-motion';
 import useStore from './useStore';
 
-const Users: FC = () => {
-  const users = useStore(store => store.users);
+type UserProps = {
+  user: string;
+};
+
+const User: FC<UserProps> = ({ user }) => {
   const userPoints = useStore(store => store.userPoints);
   const userColors = useStore(store => store.userColors);
   const userIcons = useStore(store => store.userIcons);
+
+  return (
+    <Reorder.Item dragListener={false} value={user}>
+      <div className="divider" />
+      <div className="flex justify-start items-center h-[100px] p-3">
+        <motion.div
+          className="flex items-center justify-end h-full"
+          style={{
+            backgroundColor: userColors[user],
+          }}
+          initial={{
+            width: 0,
+          }}
+          animate={{
+            width: `${((userPoints[user] || 0) / 1000) * 100}%`,
+          }}
+          transition={{
+            duration: 0.5,
+          }}
+        >
+          {(userPoints[user] || 0) > 500 && (
+            <h2 className="flex items-center px-3 m-0 text-lg text-black">
+              <img
+                src={userIcons[user]}
+                alt={user}
+                className="w-12 h-12 mt-0 mb-0 mr-4"
+              />
+              {user}
+            </h2>
+          )}
+        </motion.div>
+        {(userPoints[user] || 0) <= 500 && (
+          <h2 className="flex items-center px-3 m-0 text-lg text-white">
+            <img
+              src={userIcons[user]}
+              alt={user}
+              className="w-12 h-12 mt-0 mb-0 mr-4"
+            />
+            {user}
+          </h2>
+        )}
+      </div>
+    </Reorder.Item>
+  );
+};
+
+const Users: FC = () => {
+  const users = useStore(store => store.users);
   const setUsers = useStore(store => store.setUsers);
+  const winners = useStore(store => store.winners);
 
   return (
     <Reorder.Group
@@ -17,49 +69,14 @@ const Users: FC = () => {
       className="pl-0 list-none"
     >
       <AnimatePresence>
-        {users.map(item => (
-          <Reorder.Item dragListener={false} key={item} value={item}>
-            <div className="divider" />
-            <div className="flex justify-start items-center h-[100px] p-3">
-              <motion.div
-                className="flex items-center justify-end h-full"
-                style={{
-                  backgroundColor: userColors[item],
-                }}
-                initial={{
-                  width: 0,
-                }}
-                animate={{
-                  width: `${((userPoints[item] || 0) / 1000) * 100}%`,
-                }}
-                transition={{
-                  duration: 0.5,
-                }}
-              >
-                {(userPoints[item] || 0) > 500 && (
-                  <h2 className="flex items-center px-3 m-0 text-lg text-black">
-                    <img
-                      src={userIcons[item]}
-                      alt={item}
-                      className="w-12 h-12 mt-0 mb-0 mr-4"
-                    />
-                    {item}
-                  </h2>
-                )}
-              </motion.div>
-              {(userPoints[item] || 0) <= 500 && (
-                <h2 className="flex items-center px-3 m-0 text-lg text-white">
-                  <img
-                    src={userIcons[item]}
-                    alt={item}
-                    className="w-12 h-12 mt-0 mb-0 mr-4"
-                  />
-                  {item}
-                </h2>
-              )}
-            </div>
-          </Reorder.Item>
+        {winners.map(user => (
+          <User key={user} user={user} />
         ))}
+        {users
+          .filter(user => !winners.includes(user))
+          .map(user => (
+            <User key={user} user={user} />
+          ))}
       </AnimatePresence>
     </Reorder.Group>
   );
